77a6dd28668f9eda4ab13f22498b06c0f6171781
Alexander Pohl <alex@ahpohl.com>
2019-06-01 16:11:11 +0200

    separate public and private methods

--- pulse.h
+++ pulse.h
@@ -17,6 +17,13 @@ private:
 	const char * RRDAddress;
     const char * RRDFile;
 
+	// sensor methods
+	unsigned short Crc16(unsigned char * data_p, int length);
+    void ConfigureSerialPort(unsigned char vmin, unsigned char vtime);
+    void SendCommand(unsigned char * command, int command_length);
+    bool SyncPacket(void);
+    void ReceivePacket(unsigned char * packet, int buffer_size);
+
 public:
 	Pulse(const char * serial_device, const char * rrd_file, 
 		double meter_reading, int rev_per_kWh);
@@ -24,17 +31,12 @@ public:
 	void SetDebug(void);
 
 	// sensor methods
-	unsigned short Crc16(unsigned char * data_p, int length);
-	void ConfigureSerialPort(unsigned char vmin, unsigned char vtime);
-	void SendCommand(unsigned char * command, int command_length);
 	void SyncSerial(void);
-	bool SyncPacket(void);
-	void ReceivePacket(unsigned char * packet, int buffer_size);
 	void SetRawMode(void);
 	void SetTriggerMode(short int trigger_level_low, short int trigger_level_high);
 	int ReadSensorValue(void);
 
-	// RRD data methods
+	// rrd methods
 	void RRDConnect(const char * daemon_address);
 	void RRDCreate(void);
 	void RRDUpdateEnergyCounter(void);
