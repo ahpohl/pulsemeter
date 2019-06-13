9f09f3a7f85e39f539fa387ad35a0a7ca886a448
Alexander Pohl <alex@ahpohl.com>
2019-06-13 18:49:09 +0200

    working bash upload script to PVOutput

--- resources/pvoutput.sh
+++ resources/pvoutput.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 # PV output
 api=212dc3361019148fdb63eb0ba53b8d2dfcc4e2ec
@@ -9,17 +9,25 @@ url=https://pvoutput.org/service/r2/addstatus.jsp
 rrd=/var/lib/rrdcached/pulse.rrd
 rrdcached=unix:/run/rrdcached/rrdcached.sock
 
-# pulse
+# counter resolution
 rev_per_kwh=75
-counts=$(rrdtool lastupdate $rrd --daemon $rrdcached | tail -n 1 | cut -f2 -d " ")
-energy=$(echo "scale=3; $counts/$rev_per_kwh*1000" | bc)
+
+# fetch data at current time minus 5 minutes
+fetch_time=$(date +%H:%M --date '-5 min')
+fetch=$(rrdtool fetch /var/lib/rrdcached/pulse.rrd LAST --daemon unix:/run/rrdcached/rrdcached.sock --start $fetch_time --end $fetch_time)
 
 # pulse date and time
-epoch=$(rrdtool lastupdate $rrd --daemon $rrdcached | tail -n 1 | cut -f1 -d ":")
+epoch=$(echo $fetch | cut -f1 -d ":" | cut -f3 -d " ")
 date=$(date -d @$epoch +%Y%m%d)
 time=$(date -d @$epoch +%H:%M)
 
-# upload
-curl -d "d=$date" -d "t=$time" -d "v3=$energy" -d "c1=1" -H "X-Pvoutput-Apikey: $api" -H "X-Pvoutput-SystemId: $sys_id" $url
-echo "\n"
+# calculate energy
+counts=$(echo $fetch | cut -f4 -d " ")
+energy=$(awk 'BEGIN { printf "%.10e", '$counts' / '$rev_per_kwh' * 1000 }')
 
+# calculate power
+power=$(echo $fetch | cut -f5 -d " ")
+
+# upload
+curl -d "d=$date" -d "t=$time" -d "v3=$energy" -d "v4=$power" -d "c1=1" -H "X-Pvoutput-Apikey: $api" -H "X-Pvoutput-SystemId: $sys_id" $url
+echo
