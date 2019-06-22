9992f22c8bbc1dea2b92dff582cae3b6b21c144b
Alexander Pohl <alex@ahpohl.com>
2019-06-22 22:04:49 +0200

    pvoutput script with xport working

--- resources/pvoutput.sh
+++ resources/pvoutput.sh
@@ -11,25 +11,25 @@ rrdcached=unix:/run/rrdcached/rrdcached.sock
 
 # counter resolution
 rev_per_kwh=75
+step_size=300
 
 # fetch data at current time minus 6 minutes
 fetch_time=$(date +%H:%M --date '-6 min')
-fetch=$(rrdtool fetch /var/lib/rrdcached/pulse.rrd LAST --daemon unix:/run/rrdcached/rrdcached.sock --resolution 300 --start $fetch_time --end $fetch_time)
+
+# calculate energy
+energy_json=$(rrdtool xport --daemon $rrdcached --step $step_size --start e-$step_size --end $fetch_time --json  "DEF:counts=$rrd:energy:LAST" "CDEF:energy_kwh=counts,$rev_per_kwh,/" "CDEF:energy=energy_kwh,1000,*" XPORT:energy)
+energy=$(echo $energy_json | jq '.data[0][0]')
+
+# calculate power
+power_json=$(rrdtool xport --daemon $rrdcached --step $step_size --start e-$step_size --end $fetch_time --json "DEF:value=$rrd:power:AVERAGE" "CDEF:power=value,48000,*" XPORT:power)
+power=$(echo $power_json | jq '.data[0][0]')
 
 # pulse date and time
-epoch=$(echo $fetch | cut -f1 -d ":" | cut -f3 -d " ")
+epoch=$(echo $power_json | jq '.meta.end')
 date=$(date -d @$epoch +%Y%m%d)
 time=$(date -d @$epoch +%H:%M)
 
-# calculate energy
-counts_energy=$(echo $fetch | cut -f4 -d " ")
-energy=$(echo $fetch | awk 'BEGIN { printf "%.3f", '$counts_energy' / '$rev_per_kwh' * 1000 }')
-
-# calculate power
-counts_power=$(echo $fetch | cut -f5 -d " ")
-power=$(awk 'BEGIN { printf "%.3f", '$counts_power' * 3600 * 1000 / '$rev_per_kwh' }')
-
 # upload
-curl -d "d=$date" -d "t=$time" -d "v3=$energy" -d "c1=1" -H "X-Pvoutput-Apikey: $api" -H "X-Pvoutput-SystemId: $sys_id" $url
+curl -d "d=$date" -d "t=$time" -d "v3=$energy" -d "v4=$power" -d "c1=1" -H "X-Pvoutput-Apikey: $api" -H "X-Pvoutput-SystemId: $sys_id" $url
 echo
 echo "Date: $date $time, Energy: $energy Wh, Power: $power W, Sys Id: $sys_id"
